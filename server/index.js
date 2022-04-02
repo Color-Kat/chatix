70f68b7b759c10c1ab001bf00f876fb83a5edbe5
Color-Kat <katik7979@gmail.com>
2022-04-02 21:44:25 +0300

    simplest server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.send("Hello server");
+});
+
+io.on('connection', socket => {
+    socket.on('message', (data) => {
+        io.emit('message', data);
+    });
+});
+
+http.listen(4000, function () {
+    console.log('Listening on port 4000');
+})
