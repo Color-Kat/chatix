513e18418ada24721bdcd9b9a98e8f99437ea7f5
Color-Kat <katik7979@gmail.com>
2022-04-03 13:59:12 +0300

    react 18

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    res.send("Hello server");
-});
+// app.get('/', function(req, res){
+//     res.send("Hello server");
+// });
 
 io.on('connection', socket => {
     socket.on('message', (data) => {
