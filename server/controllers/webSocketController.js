057687b79b964fd0faeff9b0c5828cad3587251a
Color-Kat <katik7979@gmail.com>
2022-04-06 21:39:58 +0300

    chat controller

--- server/controllers/webSocketController.js
+++ server/controllers/webSocketController.js
@@ -6,6 +6,7 @@ import commonjsVariables from 'commonjs-variables-for-esmodules';
 import jwt from "jsonwebtoken";
 import { secret } from "../config/jwt_secret.js";
 import messageController from "./messageController.js";
+import checkAuth from "../checkAuth.js";
  
 const {__dirname,} = commonjsVariables(import.meta);
 
@@ -22,21 +23,34 @@ class WebSocketController {
         this.socket = socket;
         this.io = io;
 
-        socket.on('createRoom', () => messageController.createRoom());
-        socket.on('getMessages', () => messageController.getMessages());
-        socket.on('message', (value) => this.handleMessage(value));
+        socket.on('messages_of', (data) => this.getMessages(data));
+        socket.on('chat_message', (data) => this.chatMessage(data));
         socket.on('disconnect', () => this.disconnect());
+        
+    }
+
+    chatMessage(data) {
+        // Get userId of sender
+        const userId = checkAuth(data.authorization_token).id;
+        if (!userId) { return; }
+        console.log('- New message from: ' + userId + '\n- To: ' + data.to + '\n- Message: ' + data.message);
+
+        // Join user to room by userId
+        this.socket.join(userId);
+
+        messageController.addMessage(data); // Add message to db
+        this.io.to(data.to).emit('chat_message', data); // Send message to client to userId
     }
 
     
 
-    disconnect() {
-        // users.delete(this.socket);
-    }
+    disconnect() { console.log('User disconnected'); }
 }
 
 function chat(io) {
     io.on('connection', socket => {
+        console.log('User connected');
+
         new WebSocketController(io, socket);
     })
 }
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import { Server } from 'socket.io';
 import authController from "./controllers/authController.js";
 import checkAuth from './checkAuth.js';
 import messageController from './controllers/messageController.js';
+import chat from './controllers/webSocketController.js';
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -26,32 +27,7 @@ app.post('/login', authController.login);
 
 app.post('/users', authController.getUsers);
 
-
-
-io.on('connection', async (socket) => {
-    console.log('User connected');
-    
-
-    socket.on('chat_message', (data) => {
-        const userId = checkAuth(data.authorization_token).id;
-        socket.join(userId);
-        console.log(userId);
-
-        // if (!userId) {
-        //     io.emit('error', "Вы не авторизованы");
-        //     return;
-        // }
-
-        messageController.addMessage(data);
-        io.to(data.to).emit('chat_message', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
-
-
+chat(io);
 
 httpServer.listen(4000, function () {
     console.log('Listening on port 4000');
