35c169cfca2900f57d01341ca242f748ac949c50
Color-Kat <katik7979@gmail.com>
2022-04-11 13:51:47 +0300

    login form

--- client/src/components/pages/Auth.tsx
+++ client/src/components/pages/Auth.tsx
@@ -16,7 +16,7 @@ export const LoginField: FunctionComponent<LoginFieldProps> = ({ type, name, val
             name={name}
             value={value}
             onChange={e => handleChange(e, name)}
-            className="bg-black bg-opacity-25 outline-none"
+            className="w-full my-1 bg-black bg-opacity-25 rounded-xl h-10 text-sm tracking-widest py-3 px-4 outline-none"
         />
     );
 }
@@ -41,12 +41,15 @@ export const Auth: FunctionComponent<AuthProps> = () => {
         console.log(data);
     }
 
+    const login = () => { }
+    const register = () => { }
+
 
     return (
         <section id="auth" className="flex justify-center items-center w-full h-full flex-wrap container">
             {action === 'login' &&
                 <section id="login" className="w-full flex flex-col">
-                    <h1 className="text-3xl tracking-wide">Войти</h1>
+                    <h1 className="text-3xl tracking-wide mb-2.5">Войти</h1>
                     <LoginField
                         type="text"
                         placeholder="Ник"
@@ -62,11 +65,24 @@ export const Auth: FunctionComponent<AuthProps> = () => {
                         value={data.password}
                         handleChange={onFormChange}
                     />
+
+                    <div className="login__control flex justify-between items-center mt-3">
+                        <button
+                            onClick={() => { setAction('register') }}
+                            className="underline-offset-1 underline tracking-wider"
+                        >Регистрация</button>
+
+                        <button
+                            onClick={login}
+                            className="bg-app-blue py-2.5 px-7 rounded-xl text-base tracking-wider"
+                        >Войти</button>
+                    </div>
                 </section>
             }
 
             {action === 'register' &&
                 <section id="register">
+                    <h1 className="text-3xl tracking-wide mb-2.5">Регистрация</h1>
                     register
                 </section>
             }
