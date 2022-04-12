4a363aa66b8db9ddf42aafc1ef509342b7fe2d7f
Color-Kat <katik7979@gmail.com>
2022-04-12 21:10:59 +0300

    auto resize textarea

--- client/src/components/elements/Main.jsx
+++ client/src/components/elements/Main.jsx
@@ -5,7 +5,7 @@ import { Footer } from "./Footer";
 
 export const Main = ({ children }) => {
     return (
-        <div id="main" className="flex-1 flex overflow-scroll no-scrollbar">
+        <div id="main" className="flex-1 flex w-full overflow-scroll no-scrollbar">
             {/* <div id="content" className="flex-auto flex flex-col justify-between z-10 overflow-scroll overflow-x-hidden"> */}
             {children}
             {/* </div> */}
--- client/src/components/elements/MessageField.tsx
+++ client/src/components/elements/MessageField.tsx
@@ -1,5 +1,5 @@
-import { FunctionComponent, useContext, useEffect, useState } from "react";
-
+import { FunctionComponent, useContext, useEffect, useRef, useState } from "react";
+import { IoSend } from 'react-icons/io5';
 
 interface MessageFieldProps {
 
@@ -7,15 +7,52 @@ interface MessageFieldProps {
 
 export const MessageField: FunctionComponent<MessageFieldProps> = () => {
 
+    const textArea = useRef<HTMLTextAreaElement>(null);
 
+    const textAreaAutoResize = () => {
+        if (textArea.current) {
+            const textAreaElem = textArea.current;
+            function pxToNumber(px: string) { return +px.slice(0, px.length - 2) }
+
+            // If height == scrollHeight try to reset text area height
+            // it is for auto decrease height
+            if (textAreaElem.scrollHeight === textAreaElem.offsetHeight) {
+                textAreaElem.style.height = '40px';
+            }
+
+            // Increase textArea height when scroll decreases
+            if (pxToNumber(textAreaElem.style.height) < pxToNumber(textAreaElem.style.maxHeight))
+                textAreaElem.style.height = textAreaElem.scrollHeight + 'px';
+        }
+    }
+
+    const changeHandle = () => {
+        textAreaAutoResize();
+
+
+    }
 
     useEffect(() => {
 
     }, []);
 
     return (
-        <div className="message-field bg-red-500 w-full flex">
-            123
+        <div className="message-field w-full flex h-8 sticky bottom-12 justify-between items-end">
+            <textarea
+                ref={textArea}
+                onChange={changeHandle}
+                name="message"
+                id="message-field"
+                placeholder="Сообщение"
+                style={{
+                    minHeight: 40,
+                    maxHeight: 160
+                }}
+                className="flex flex-1 bg-app-dark rounded-lg py-2 px-2.5 text-base tracking-wider outline-none resize-none"
+            ></textarea>
+            <button
+                className="h-10 w-10 bg-app-light flex items-center justify-center rounded-lg"
+            ><IoSend size={16} opacity="55" color="white" /></button>
         </div>
     );
 }
